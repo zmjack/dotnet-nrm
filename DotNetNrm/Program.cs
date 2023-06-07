e91c8d0a5fec27ef409323de5f614c5465891c7f
zmjack <jack@zmland.com>
2023-06-07 16:28:42 +0800

    DotNetNrm.0.2.1

--- DotNetNrm/Program.cs
+++ DotNetNrm/Program.cs
@@ -19,7 +19,11 @@ namespace DotNetNrm
             if (!OperatingSystem.IsWindows()) throw new PlatformNotSupportedException("Only windows is supported.");
 
 #pragma warning disable CA1416 // Validate platform compatibility
-            Console.BufferHeight = 9001;
+            try
+            {
+                Console.BufferHeight = 9001;
+            }
+            catch { }
 #pragma warning restore CA1416 // Validate platform compatibility
 
             var rootCommand = new RootCommand(".NET tool of NPM registry manager.");
